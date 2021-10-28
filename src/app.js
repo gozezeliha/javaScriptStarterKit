8fba81b2453cf952a35798ed696c4e0b31bc84de
Zeliha GÖZE <55953109+gozezeliha@users.noreply.github.com>
2021-10-28 18:21:50 +0300

    javascrpit-ders1

---
+++ src/app.js
@@ -0,0 +1,17 @@
+console.log("Merhaba Dünya")
+
+var dolarDun = 9.20
+
+var dolarBugun = 9.30
+
+console.log(dolarBugun)
+
+//array
+let konutKredileri = ["Konut Kredisi","Kamu Konut Kredisi","Emlak Konuk Kredisi"]
+
+console.log("<ul>")
+for (let index = 0; index < konutKredileri.length; index++) {
+    console.log("<li>"+konutKredileri[index]+"</li>")    
+  
+}
+console.log("</ul>");
